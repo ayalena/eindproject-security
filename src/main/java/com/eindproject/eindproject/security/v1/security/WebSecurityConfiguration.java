d4ee67766f9b0525db53dec3e4cd162bb0ac5536
ayalena <82506563+ayalena@users.noreply.github.com>
2021-10-30 19:48:50 +0200

    created users in websecurity

---
+++ src/main/java/com/eindproject/eindproject/security/v1/security/WebSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.eindproject.eindproject.security.v1.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    //creating users
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER","ADMIN")
+                ;
+    }
+
+
+
+}
