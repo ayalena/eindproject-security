12d34703648138b0160e78496cf31aff6b0094db
ayalena <82506563+ayalena@users.noreply.github.com>
2021-10-30 16:44:30 +0200

    first commit

---
+++ src/main/java/com/eindproject/eindproject/security/v1/EindprojectSecurityV1Application.java
@@ -0,0 +1,13 @@
+package com.eindproject.eindproject.security.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindprojectSecurityV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindprojectSecurityV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eindproject/eindproject/security/v1/EindprojectSecurityV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindproject.eindproject.security.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindprojectSecurityV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
