52141ad85ab85b86d07670264b0e9a6147307ac5
ayalena <82506563+ayalena@users.noreply.github.com>
2021-11-01 17:59:55 +0100

    bcrypt password

--- src/main/java/com/eindproject/eindproject/security/v1/security/WebSecurityConfiguration.java
+++ src/main/java/com/eindproject/eindproject/security/v1/security/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.eindproject.eindproject.security.v1.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -17,6 +20,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     //creating users
     @Override
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,7 @@
 INSERT INTO users (username, password, enabled)
 VALUES
-('user', '{noop}password', TRUE),
-('admin', '{noop}password', TRUE);
---in plaats hiervan gaan we encrypted ww gebruiken:
---('user', '$2a$12$/qvUQlMHbJ0mc.BYL.NUme41on.RaDNkPIswk6Md1mAduUdvemnKi', TRUE),
---('admin', '$2a$12$/qvUQlMHbJ0mc.BYL.NUme41on.RaDNkPIswk6Md1mAduUdvemnKi', TRUE);
+('user', '$2a$12$2CSgm1e2tLltFx9ltNc/puAT1YRdd9qLXdYtRecoJWDGEYwjCK0FK', TRUE),
+('admin', '$2a$12$A4hm/D8B6jYtFscTBBf9Q.OhzLeK9773U925KKiL9jhNi.fFvsxm2', TRUE);
 
 INSERT INTO authorities (username, authority)
 VALUES
